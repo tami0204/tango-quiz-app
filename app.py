d2fc15bee7edea3f1ba0fcb3221e7e961f38d522
tami0204 <40409281+tami0204@users.noreply.github.com>
2025-07-09 21:01:38 +0900

    Update app.py

--- app.py
+++ app.py
@@ -22,7 +22,8 @@ class QuizApp:
         for key, val in self.defaults.items():
             st.session_state[key] = val 
             if not isinstance(val, set) :
-            else set()
+            else: 
+                set()
 
     def filter_data(self):
         field = st.selectbox("分野を選ぶ", ["すべて"] + sorted(self.df["分野"].dropna().unique()))
